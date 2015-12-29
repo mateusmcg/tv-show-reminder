a2b6d87cb27ad3b09be238582a6a51355fa1760d
Mateus Cerqueira <mateus_mcg@icloud.com>
2015-12-28 23:44:11 -0200

    #3 and #4 - v1 Layout defined with horizontal menu. Added a partial way for the user to change the app-style

--- app/common/nav/header-ctrl.js
+++ app/common/nav/header-ctrl.js
@@ -3,7 +3,7 @@
 angular.module('app.common.nav.header', []);
 
 angular.module('app.common.nav.header')
-    .controller('HeaderCtrl', ['$scope', '$log', 'SearchRestService', function ($scope, $log, SearchRestService) {
+    .controller('HeaderCtrl', ['$scope', '$log', '$location', 'SearchRestService', function ($scope, $log, $location, SearchRestService) {
 
         var vm = this;
 
@@ -15,10 +15,33 @@ angular.module('app.common.nav.header')
         vm.getResults = getResults;
         vm.clearSearch = clearSearch;
         vm.selectItem = selectItem;
+        vm.changeStyle = changeStyle;
+        
+        function changeStyle(){
+            var targetelement = 'link'; //determine element type to create nodelist using
+            var targetattr = 'href'; //determine corresponding attribute to test for
+            var allsuspects=document.getElementsByTagName(targetelement)
+            for (var i=allsuspects.length; i>=0; i--){ //search backwards within nodelist for matching elements to remove
+                if (allsuspects[i] && allsuspects[i].getAttribute(targetattr)!=null && allsuspects[i].getAttribute(targetattr).indexOf('css/app-theme.css')!=-1){
+                    var newelement = createCssFile('themes/pink-theme.css', 'css')
+                    allsuspects[i].parentNode.replaceChild(newelement, allsuspects[i])
+                }
+            }
+        }
+        
+        function createCssFile(filename){
+            var fileref = document.createElement("link")
+            fileref.setAttribute("rel", "stylesheet")
+            fileref.setAttribute("type", "text/css")
+            fileref.setAttribute("href", "assets/" + filename)
+            return fileref
+        }
         
         function selectItem(){
             var selectedItem = vm.searchQuery;
             $log.debug('Selected Item: ', selectedItem);
+            
+            $location.path('/show').search(selectedItem);
         }
         
         function clearSearch(){
@@ -39,16 +62,16 @@ angular.module('app.common.nav.header')
                                 var images = value.show.images.poster;
                                 var img = images.thumb ? images.thumb : images.medium ? images.medium : images.full ? images.full : null;
                                 var year = value.show.year ? ' (' + value.show.year + ')' : '';
-                                results.push({name: value.show.title + year, img: img, group: 'Shows', ids: value.show.ids});
+                                results.push({name: value.show.title + year, img: img, group: 'Shows', id: value.show.ids.trakt});
                             } else if(value.type === 'movie'){
                                 var images = value.movie.images.poster;
                                 var img = images.thumb ? images.thumb : images.medium ? images.medium : images.full ? images.full : null;
                                 var year = value.movie.year ? ' (' + value.movie.year + ')' : '';
-                                results.push({name: value.movie.title + year, img: img, group: 'Movies', ids: value.movie.ids});
+                                results.push({name: value.movie.title + year, img: img, group: 'Movies', id: value.movie.trakt});
                             } else{
                                 var images = value.person.images.headshot;
                                 var img = images.thumb ? images.thumb : images.medium ? images.medium : images.full ? images.full : null;
-                                results.push({name: value.person.name, img: img, group: 'People', ids: value.person.ids});
+                                results.push({name: value.person.name, img: img, group: 'People', id: value.person.trakt});
                             }
                         });
                         
